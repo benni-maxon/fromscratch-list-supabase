49f3088d41acc7121172bcb53dc358bc6fda0443
benni maxon <benni.maxon@gmail.com>
2022-11-14 15:54:41 -0800

    built window.addEventListener in app.js

--- app.js
+++ app.js
@@ -1,10 +1,21 @@
 /* Imports */
 
+import { getCards } from './fetch-utils.js';
+
 /* Get DOM Elements */
+
+const cardListContainer = document.getElementById('card-list-container');
 
 /* State */
 
 /* Events */
+window.addEventListener('load', async () => {
+    const cards = await getCards();
+    for (let card of cards) {
+        const cardEl = renderCardTemplate(card);
+        cardListContainer.append(cardEl);
+    }
+});
 
 /* Display Functions */
 
