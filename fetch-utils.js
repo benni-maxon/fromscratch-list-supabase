5b30441e47929cb843639c6bc5c54a0d4dffa33c
benni maxon <benni.maxon@gmail.com>
2022-11-14 15:43:13 -0800

    added supabase client info and built getCards()

---
+++ fetch-utils.js
@@ -0,0 +1,10 @@
+const SUPABASE_URL = 'https://gxwgjhfyrlwiqakdeamc.supabase.co';
+const SUPABASE_KEY =
+    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoiYW5vbiIsImlhdCI6MTYzNjQxMTMxMiwiZXhwIjoxOTUxOTg3MzEyfQ.PHekiwfLxT73qQsLklp0QFEfNx9NlmkssJFDnlvNIcA';
+
+const client = supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
+
+export async function getCards() {
+    const response = await client.from('mtg_decklist').select('*');
+    return response.data;
+}
