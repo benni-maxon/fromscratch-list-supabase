83ad04dd6218129fc1059555c742e42be59b3d16
benni maxon <benni.maxon@gmail.com>
2022-11-14 16:20:03 -0800

    cleaned up comments in app.js

--- app.js
+++ app.js
@@ -1,14 +1,11 @@
 /* Imports */
-
 import { getCards } from './fetch-utils.js';
+import { renderCardTemplate } from './render-utils.js';
 
 /* Get DOM Elements */
-
 const cardListContainer = document.getElementById('card-list-container');
 
-/* State */
-
-/* Events */
+/* Display Functions */
 window.addEventListener('load', async () => {
     const cards = await getCards();
     for (let card of cards) {
@@ -16,7 +13,5 @@ window.addEventListener('load', async () => {
         cardListContainer.append(cardEl);
     }
 });
-
-/* Display Functions */
 
 // (don't forget to call any display functions you want to run on page load!)
